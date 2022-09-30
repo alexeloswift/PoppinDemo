c35c9827e042f4e9c8e70b0a3830be03be8feac6
Alexis Diaz <alexelo.swift@gmail.com>
2022-09-30 15:44:28 -0400

    Initial Commit

---
+++ PoppinDemo/PoppinDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoppinDemoApp.swift
+//  PoppinDemo
+//
+//  Created by Alexis Diaz on 9/30/22.
+//
+
+import SwiftUI
+
+@main
+struct PoppinDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
